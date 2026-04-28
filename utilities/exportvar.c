e39104aa399196a8876439c752d352d543a2da68
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-04-28 20:06:59 +0200

    Finally compiles with MSVC wow

--- utilities/exportvar.c
+++ utilities/exportvar.c
@@ -429,3 +429,47 @@ MFILE *ExportVar(LPCALC lpCalc, TCHAR* fn, symbol83P_t* sym) {
 	mputc((chksum >> 8) & 0xFF, outfile);
 	return outfile;
 }
+
+
+TCHAR type_ext[][4] = {
+	_T("8xn"),
+	_T("8xl"),
+	_T("8xm"),
+	_T("8xy"),
+	_T("8xs"),
+	_T("8xp"),
+	_T("8xp"),
+	_T("8xi"),
+	_T("8xd"),
+	_T(""),
+	_T("8xy"),
+	_T("8xy"),
+	_T("8xc"),
+	_T("8xl"),
+	_T(""),
+	_T("8xw"),
+	_T("8xz"),
+	_T("8xt"),
+	_T(""),
+	_T("8xb"),
+	_T("8xk"),
+	_T("8xv"),
+	_T(""),
+	_T("8xg"),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T(""),
+	_T("8xy"),
+	_T(""),
+	_T(""),
+	_T("")
+};
